7cd9f94ee3844cf5fca4ac14cf82df7c1737cfb4
anandanfd <ananda.ngestitama.aim@gmail.com>
2019-02-24 13:55:12 +0700

    commit 1

---
+++ B01-16318161-01.cpp
@@ -0,0 +1,39 @@
+// NIM /Nama 	: Ananda Ngestitama Faradewi
+// Tanggal		: 19 Februari 2019
+// Deskripsi 	: Membuat Program yang dapat menentukan game yang cocok untuk pengguna secara interaktif
+
+#include <iostream>
+using namespace std;
+int main () 
+{
+	
+	int N;
+	
+	string YES;
+	
+	string NO;
+	
+	string REAL;
+	
+	cout << " Do you like Video games? " << endl ;
+	cout << "YES/NO" << endl ;
+	cin >> YES;
+	cout << " Buy a PC " << endl;
+	
+
+	cout << " Do you like Video games? " << endl ;
+	cin >> NO;
+
+		cout << " Do you have any friends? " << endl ;
+		cin >> NO ;
+		cout << " But a PS3" << endl ;
+		
+		cout << " Do you like video games?" << endl ;
+		cin >> NO ;
+		cout << " Do you have any friends? " << endl ;
+		cin >> YES ;
+		cout << " Real friend or Internet Friends? " << endl ;
+		cin >> REAL ;
+		cout << " Buy a wifi " << endl ;
+	}
+	
